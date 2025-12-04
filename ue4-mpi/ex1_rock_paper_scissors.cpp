2329d1eda5ea4b1c8e95b08d79dae9919297ed05
Yvonne Marneth <marneth.yvonne@gmail.com>
2025-12-04 08:28:41 +0100

    Reformat

--- ue4-mpi/ex1_rock_paper_scissors.cpp
+++ ue4-mpi/ex1_rock_paper_scissors.cpp
@@ -2,10 +2,10 @@
 #include <mpi.h>
 #include <random>
 
-int main__rock(int argc, char** argv)
+int main__rock(int argc, char **argv)
 {
     MPI_Init(&argc, &argv);
-    
+
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -15,12 +15,12 @@ int main__rock(int argc, char** argv)
 
     int score = 0;
 
-    std::default_random_engine gen{ 42 + (unsigned int)rank };
-    std::uniform_int_distribution<int> dist{ ROCK, SCISSORS };
+    std::default_random_engine gen{42 + (unsigned int)rank};
+    std::uniform_int_distribution<int> dist{ROCK, SCISSORS};
 
     MPI_Comm cart_comm;
-    int dims[1] = { size };
-    int periods[1] = { 1 };
+    int dims[1] = {size};
+    int periods[1] = {1};
     MPI_Cart_create(MPI_COMM_WORLD, 1, dims, periods, 0, &cart_comm);
 
     int cart_rank;
@@ -29,7 +29,8 @@ int main__rock(int argc, char** argv)
     int left_opponent, right_opponent;
     MPI_Cart_shift(cart_comm, 0, 1, &left_opponent, &right_opponent);
 
-    for (int rounds = 0; rounds < ROUNDS; rounds++) {
+    for (int rounds = 0; rounds < ROUNDS; rounds++)
+    {
         int choice = dist(gen);
         int opponent_choice;
 
@@ -82,8 +83,7 @@ int main__rock(int argc, char** argv)
             right_opponent,
             0,
             cart_comm,
-            &reqs[0]
-        );
+            &reqs[0]);
 
         MPI_Irecv(
             &opponent_choice,
@@ -92,21 +92,21 @@ int main__rock(int argc, char** argv)
             left_opponent,
             0,
             cart_comm,
-            &reqs[1]
-        );
+            &reqs[1]);
 
         MPI_Waitall(2, reqs, MPI_STATUSES_IGNORE);
 
         constexpr int score_lookup[3][3] = {
-            { 0, -1, +1 }, // ROCK versus others
-            { +1, 0, -1 }, // PAPER versus others
-            { - 1, +1, 0 } // SCISSORS versus others
+            {0, -1, +1}, // ROCK versus others
+            {+1, 0, -1}, // PAPER versus others
+            {-1, +1, 0}  // SCISSORS versus others
         };
 
         score += score_lookup[choice][opponent_choice];
     }
 
-    std::cout << "Process " << rank << ": " << score << std::endl << std::flush;
+    std::cout << "Process " << rank << ": " << score << std::endl
+              << std::flush;
 
     MPI_Finalize();
 
